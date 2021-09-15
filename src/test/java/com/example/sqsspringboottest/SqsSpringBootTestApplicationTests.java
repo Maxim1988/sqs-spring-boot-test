0296e834c8c7b6c3c777bdfe92f4f73520697d0c
maximzvyagin <Max198805@yandex.ru>
2021-09-15 11:16:12 +0300

    Sqs spring boot test create

---
+++ src/test/java/com/example/sqsspringboottest/SqsSpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqsspringboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsSpringBootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
